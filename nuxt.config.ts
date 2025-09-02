5b27be77efe2dac2e1e56f6f8f986824c2e31340
yerisaguilar <yerisaguilarg@gmail.com>
2025-09-02 11:02:27 -0700

    refactor: clean up .dockerignore and improve nuxt.config.ts formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui',["@prisma/nuxt",{ client: false }]],
-  css: [
-    "~/assets/css/index.css",'~/assets/css/main.css'
-  ],
+  modules: ["@nuxt/ui", ["@prisma/nuxt", { client: false }]],
+  css: ["~/assets/css/index.css", "~/assets/css/main.css"],
   ui: {
     // prefix: 'Nuxt',
     fonts: false,
-    colorMode: false
+    colorMode: false,
   },
 
   vite: {
-    server: {
-      watch: {
-        usePolling: true,
-        interval: 300 // puedes probar entre 100–300ms
-      },
-      hmr: {
-        host: 'localhost',
-        port: 3001
-      }
-    },
+    // server: {
+    //   watch: {
+    //     usePolling: true,
+    //     interval: 300 // puedes probar entre 100–300ms
+    //   },
+    //   hmr: {
+    //     host: 'localhost',
+    //     port: 3001
+    //   }
+    // },
     ssr: {
       // No externaliza Prisma, deja que se use solo Node
-      external: ["@prisma/client"]
+      external: ["@prisma/client"],
     },
-        resolve: {
-          alias: {
-            ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
-          }
-        },
-    
+    resolve: {
+      alias: {
+        ".prisma/client/index-browser":
+          "./node_modules/.prisma/client/index-browser.js",
+      },
+    },
+    // build: {
+  //   transpile: []
+  // }
   },
-})
+});
